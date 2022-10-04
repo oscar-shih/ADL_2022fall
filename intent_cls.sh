7e303fee0c4addcede76a9adcc38fe174da80847
Shih Min-Han <73226430+oscar-shih@users.noreply.github.com>
2022-10-04 13:04:39 +0800

    Delete intent_cls.sh

--- intent_cls.sh
+++
@@ -1,4 +0,0 @@
-# "${1}" is the first argument passed to the script
-# "${2}" is the second argument passed to the script
-# python3 test_intent.py --rnn_type lstm --num_layers 2 --test_file data/intent/test.json --ckpt_path ckpt/intent
-python3 test_intent.py --rnn_type lstm --num_layers 2 --test_file "${1}" --ckpt_path ckpt/intent --pred_file "${2}"
